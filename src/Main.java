e1d81bb1c35d69300d3532b32b692fca6ea1954b
unknown <johannjerman014@gmail.com>
2023-06-03 21:07:01 -0300

    terminando

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        int integer = 1;
+        long longest = 2;
+        double doble= 2.22;
+        Boolean boleano = true;
+        String string = "name";
+        System.out.println(integer);
+        System.out.println(longest);
+        System.out.println(doble);
+        System.out.println(boleano);
+        System.out.println(string);
 
     }
 }
